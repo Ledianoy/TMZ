ad1eb11b38e530fe2d20e9427a3dc3ca5b4161a4
sergey <Ledovskiys@gmail.com>
2021-01-10 16:02:08 +0300

    test2

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,9 +1,9 @@
+
 import sentry_sdk
 
 from framework.util.settings import get_setting
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
-
 
 def application(environ, start_response):
     if environ["PATH_INFO"] == "/e/":
@@ -30,6 +30,11 @@ def application(environ, start_response):
         b"</html>"
     )
 
+
+    web_format_encoding = payload.decode(encoding='utf-8')
+    web_text = web_format_encoding.replace('<p>This is a template project.</p>','<p>{}</p>').format(environ)
+    web_text_encod = web_text.encode(encoding='utf-8')
+    payload = web_text_encod
     start_response(status, list(headers.items()))
 
     yield payload
