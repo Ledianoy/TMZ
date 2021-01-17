2c95e72eb4c5e3071030f05522a2e68c73267662
sergey <Ledovskiys@gmail.com>
2021-01-17 16:41:34 +0300

    test4

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,40 +1,53 @@
 
 import sentry_sdk
 
+from framework.dirs import DIR_SRC
 from framework.util.settings import get_setting
 
 sentry_sdk.init(get_setting("SENTRY_DSN"), traces_sample_rate=1.0)
 
 def application(environ, start_response):
+    url_name = ""
     if environ["PATH_INFO"] == "/e/":
         division = 1 / 0
+
+    if environ["PATH_INFO"] == "/environ/":
+        url_name = "about.html"
+
+    if environ["PATH_INFO"] == "/":
+        url_name = "intex.html"
 
     status = "200 OK"
 
     headers = {
         "Content-type": "text/html",
     }
+    environ2 = ""
 
-    payload = (
-        b"<!DOCTYPE html>"
-        b"<html>"
-        b"<head>"
-        b"<title>Alpha</title>"
-        b'<meta charset="utf-8">'
-        b"</head>"
-        b"<body>"
-        b"<h1>Project Alpha</h1>"
-        b"<hr>"
-        b"<p>This is a template project.</p>"
-        b"</body>"
-        b"</html>"
-    )
+    for key in environ:
+        value = environ[key]
+        text = f"<p>{key} : {value}</p>"
+        environ2 = environ2 + text
 
 
-    web_format_encoding = payload.decode(encoding='utf-8')
-    web_text = web_format_encoding.replace('<p>This is a template project.</p>','<p>{}</p>').format(environ)
-    web_text_encod = web_text.encode(encoding='utf-8')
-    payload = web_text_encod
+    #
+    # payload = template.format(evrine=environ2)
+    if url_name == "about.html":
+        template = read_template(url_name)
+        payload = template.format(evrine=environ2)
+    else:
+        template = read_template(url_name)
+        payload = template.format()
     start_response(status, list(headers.items()))
 
-    yield payload
+    yield payload.encode()
+
+def read_template(template_name:str) ->str:
+    dir_template = DIR_SRC / "main" / "temples"
+    template = dir_template / template_name
+
+    assert template.is_file()
+
+    with template.open("r") as fd:
+        content = fd.read()
+    return content
