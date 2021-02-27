aebd23ad55d8147b03f66e30330761d814272035
sergey <Ledovskiys@gmail.com>
2021-02-27 10:00:55 +0300

    django

--- src/main/henders/henders_system.py
+++ src/main/henders/henders_system.py
@@ -11,14 +11,18 @@
 #        #  status=HTTPStatus.NOT_FOUND,
 #
 #    return response
+import traceback
+from http import HTTPStatus
 
-#
-# def handle_500(_request: RequestT) -> ResponseT:
-#     response = ResponseT(
-#         content_type="text/plain",
-#         payload=traceback.format_exc(),
-#         status=HTTPStatus.INTERNAL_SERVER_ERROR,
-#     )
+from main.costom_type import RequestT, ResponseT
+
+
+def handle_500(_request: RequestT) -> ResponseT:
+    response = ResponseT(
+        content_type="text/plain",
+        payload=traceback.format_exc(),
+        status=HTTPStatus.INTERNAL_SERVER_ERROR,
+    )
 #
 #     return response
 #
