6ec850cbfe11e717c578e8db18514b4feb4b7511
sergey <Ledovskiys@gmail.com>
2021-02-27 13:20:06 +0300

    django

--- src/main/henders/hendlers.py
+++ src/main/henders/hendlers.py
@@ -1,12 +1,12 @@
-# from django.http import HttpRequest, HttpResponse
-#
+from django.http import HttpRequest, HttpResponse
+
 # from main.henders.henders_system import handle_404
-# from project.urls import urlpatterns
-#
-#
-# def get_handler(request: HttpRequest) -> HttpResponse:
-#
-#     handler = urlpatterns.get(request.path, handle_404)
-#
-#     return handler
-#
+from project.urls import urlpatterns
+
+
+def get_handler(request: HttpRequest) -> HttpResponse:
+
+    handler = urlpatterns.get(request.path)
+
+    return handler
+
